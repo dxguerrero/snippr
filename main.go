485cab68c9cd18be704fac2cc561c7efba5e0530
Daniel Guerrero <danxguerrero@google.com>
2024-01-05 12:54:44 -0500

    Reads a json file and seeds data

--- main.go
+++ main.go
@@ -1,9 +1,44 @@
 package main
 
 import (
-	
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+
+	//"github.com/gin-gonic/gin"
 )
 
+type snippet struct {
+	ID int `json:"id"`
+	Language string `json:"language"`
+	Code string `json:"code"`
+}
+
+var snippets []snippet  
+
 func main() {
+	readFile()
+	fmt.Println(snippets)
+}
+
+func readFile() []snippet {
+	file, err := os.Open("/home/danxguerrero/snippr/seedData.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = json.Unmarshal(data, &snippets)
+	if err != nil {
+		log.Fatal(err)
+	}
 	
+	return snippets
 }
